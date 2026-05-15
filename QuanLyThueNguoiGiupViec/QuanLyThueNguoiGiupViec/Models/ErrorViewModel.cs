09d6581b43b4387416338bac51877b49ba207234
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-05-15 19:11:38 +0700

    init project

---
+++ QuanLyThueNguoiGiupViec/QuanLyThueNguoiGiupViec/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyThueNguoiGiupViec.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
